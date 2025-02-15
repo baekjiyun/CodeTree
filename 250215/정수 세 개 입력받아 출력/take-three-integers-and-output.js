9db89d80db17bfde657697a3e2b3829586dd5ca7
백지윤 <71875528+baekjiyun@users.noreply.github.com>
2025-02-15 18:55:44 +0900

    Title: 정수 세 개 입력받아 출력, Time: 180ms, Memory: 15MB, Status: Passed - CodeTree

--- 250215/정수 세 개 입력받아 출력/take-three-integers-and-output.js
+++ 250215/정수 세 개 입력받아 출력/take-three-integers-and-output.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-let input = fs.readFileSync(0).toString().split("\n"||" ");
+let input = fs.readFileSync(0).toString().trim().split(/\s+/);
 let a = input[0];
 let b = input[1];
 let c = input[2];
