e1634457564c2039e75190c1c4bae3f4e5a8d71d
백지윤 <71875528+baekjiyun@users.noreply.github.com>
2025-02-17 05:03:30 +0900

    Title: 합과 차의 나눗셈, Time: 191ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 250217/합과 차의 나눗셈/divide-of-sum-and-sub.js
@@ -0,0 +1,6 @@
+const fs = require("fs");
+let input = fs.readFileSync(0).toString().split(" ");
+let a = Number(input[0]);
+let b = Number(input[1]);
+let c = parseInt((a+b)/(a-b)).toFixed(2);
+console.log(c);
