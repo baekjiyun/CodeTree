6623e385e5d912e7dd232abca102f5b6da7c737d
백지윤 <71875528+baekjiyun@users.noreply.github.com>
2025-02-19 02:43:20 +0900

    Title: 두 번의 연산, Time: 172ms, Memory: 15MB, Status: Passed - CodeTree

---
+++ 250219/두 번의 연산/two-operations.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+let input = Number(fs.readFileSync(0).toString());
+if (input%2 === 1){
+    input+=3;
+}
+if (input%3 === 0){
+    input= parseInt(input/3);
+}
+
+console.log(input);
