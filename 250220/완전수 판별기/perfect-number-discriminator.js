155164c7bdc1752e826e9643bf03e409b2000617
백지윤 <71875528+baekjiyun@users.noreply.github.com>
2025-02-20 01:06:02 +0900

    Title: 완전수 판별기, Time: 186ms, Memory: 14MB, Status: Runtime Error - CodeTree

---
+++ 250220/완전수 판별기/perfect-number-discriminator.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+let n = Number(fs.readFileSync(0).toString());
+let sum = 0;
+for(let i = 1; i<=n;i++){
+    if(n%i===0){
+        sum+=i;
+    }
+}
+if(summ===n){
+    console.log("P");
+}
+else{
+    console.log("N");
+}
